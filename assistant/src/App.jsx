9f8a5ff6bac51e0035a4fd2599640ef678a325ba
Kristian Hole Jensen <kristianhj98@gmail.com>
2026-05-19 22:50:31 +0200

    Added some props

---
+++ assistant/src/App.jsx
@@ -0,0 +1,13 @@
+
+import ProfileCard from './ProfileCard.jsx';
+function App() {
+  return (
+    <div> Personal Digital Assistant 
+      <ProfileCard title="Alexa" handle="@alexa99"/>
+      <ProfileCard title="Cortana" handle="@cortana32"/>
+      <ProfileCard title="Siri" handle="@siri01"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ assistant/src/ProfileCard.jsx
@@ -0,0 +1,9 @@
+function ProfileCard(props) {
+    return (
+        <div>Title is {props.title}
+        <div>Handle is {props.handle}</div>
+        </div>
+    );
+}
+
+export default ProfileCard;
