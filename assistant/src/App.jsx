0dbd44f33a9c95b027c5d4cd17edca6f0e85388f
Kristian Hole Jensen <kristianhj98@gmail.com>
2026-05-21 20:16:57 +0200

    Added images

--- assistant/src/App.jsx
+++ assistant/src/App.jsx
@@ -1,11 +1,16 @@
 
 import ProfileCard from './ProfileCard.jsx';
+import AlexImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+
 function App() {
   return (
     <div> Personal Digital Assistant 
-      <ProfileCard title="Alexa" handle="@alexa99"/>
-      <ProfileCard title="Cortana" handle="@cortana32"/>
-      <ProfileCard title="Siri" handle="@siri01"/>
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexImage}/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
     </div>
   );
 }
--- assistant/src/ProfileCard.jsx
+++ assistant/src/ProfileCard.jsx
@@ -1,8 +1,10 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image}) {
 
     return (
-        <div>Title is {title}
-        <div>Handle is {handle}</div>
+        <div>
+            <img src={image} alt={title} />
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
     );
 }
